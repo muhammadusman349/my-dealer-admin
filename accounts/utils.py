51e4f75a9f02cf933fbadb8fc6cc438601417261
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2023-08-31 15:55:51 +0500

    add dealer choices

--- accounts/utils.py
+++ accounts/utils.py
@@ -23,10 +23,27 @@ Sales_representative_choices = [
 
 ]
 
-user_choices=[('admin',"Admin"),("dealer","Dealer"),("repair_facility","Repair Facility"),("sales_representative","Sales Representative"),("agency","Agency")]
+
+Dealer_choices = [
+                    ('can_view_role','can view role'),
+                    ('can_add_role','can add role'),
+                    ('can_update_role','can update role'),
+                    ('can_delete_role','can delete role'),
+
+                    ('can_view_Dealer_member','can view Dealer member'),
+                    ('can_add_Dealer_member','can add Dealer member'),
+                    ('can_update_Dealer_member','can update Dealer member'),
+                    ('can_delete_Dealer_member','can delete Dealer member'),
+
+]
+
+
+user_choices=[('admin',"Admin"),('dealer','Dealer'),('repair_facility','Repair Facility'),('sales_representative','Sales Representative'),('agency','Agency')]
 
 producer_type = [('franchise','Franchise'),('independent','Independent'),('bank','Bank'),('credit union','Credit Union')]
 
 claim_review_choices = [('oem','OEM'),('after market','After Market')]
 
 payment_method_choices = [('stripe','Stripe'),('dwolla','Dwolla')]
+ 
+dealer_of = [('admin','Admin'),('agency','Agency')]
